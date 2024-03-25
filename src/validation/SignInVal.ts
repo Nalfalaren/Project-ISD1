3593c26ac0aadb95307f733219770e38c4d81489
Chu Tien Tuan <hieuchuu138@gmail.com>
2024-03-25 21:57:59 +0700

    "fix"

--- src/validation/SignInVal.ts
+++ src/validation/SignInVal.ts
@@ -1,7 +1,6 @@
 import { z, ZodType } from 'zod';
 import { IUserInfo } from '../interface/IUSerInfo';
 
-
 const schema: ZodType<IUserInfo> = z.object({
   username: z.string().min(1, {message: 'Email không được để trống!'}),
   password: z.string().min(8, {message: 'Mật khẩu phải trên 8 kí tự'}),
